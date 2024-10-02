b3f9e1ac1ef5dc1c678695dfc42dc4ca62da7112
Olaf Wintermann <olaf.wintermann@gmail.com>
2024-10-02 18:47:16 +0200

    add collection copy test

--- davtest/tests/copy.py
+++ davtest/tests/copy.py
@@ -8,10 +8,25 @@ class TestCopy(davtest.test.WebdavTest):
         self.create_testdata('copy1', 1)
 
         destination = self.http.get_uri('/webdavtests/copy1/res0_new')
-        res = self.http.doRequest('COPY', '/webdavtests/copy1/res0', 'testcontent', hdrs={'Destination': destination})
+        res = self.http.doRequest('COPY', '/webdavtests/copy1/res0', hdrs={'Destination': destination})
 
         if res.status > 299:
             raise Exception(f'COPY status code: {res.status}')
 
         if not davtest.webdav.resource_exists(self.http, '/webdavtests/copy1/res0_new'):
+            raise Exception('resource does not exist')
+
+    def test_copy_collection(self):
+        self.create_testdata('copy2', 1)
+
+        destination = self.http.get_uri('/webdavtests/copy2_new')
+        res = self.http.doRequest('COPY', '/webdavtests/copy2/', hdrs={'Destination': destination})
+
+        if res.status > 299:
+            raise Exception(f'COPY status code: {res.status}')
+
+        if not davtest.webdav.resource_exists(self.http, '/webdavtests/copy2_new/'):
+            raise Exception('collection does not exist')
+
+        if not davtest.webdav.resource_exists(self.http, '/webdavtests/copy2_new/res0'):
             raise Exception('resource does not exist')
