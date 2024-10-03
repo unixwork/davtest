10465dd830eba42a19ae3146039818dedf4d90bd
Olaf Wintermann <olaf.wintermann@gmail.com>
2024-10-03 17:56:55 +0200

    add copy override test

--- davtest/tests/copy.py
+++ davtest/tests/copy.py
@@ -30,3 +30,28 @@ class TestCopy(davtest.test.WebdavTest):
 
         if not davtest.webdav.resource_exists(self.http, '/webdavtests/copy2_new/res0'):
             raise Exception('resource does not exist')
+
+    def test_copy_override_noheader(self):
+        self.create_testdata('copy3', 1)
+        self.create_testdata('copy3_target', 1)
+
+        res = self.http.doRequest('PUT', '/webdavtests/copy3/res0', 'copy testcontent 1')
+        if res.status > 299:
+            raise Exception('failed to write test data 1')
+
+        res = self.http.doRequest('PUT', '/webdavtests/copy3_target/res0', 'target testcontent 1')
+        if res.status > 299:
+            raise Exception('failed to write test data 2')
+
+        destination = self.http.get_uri('/webdavtests/copy3_target/')
+        res = self.http.doRequest('COPY', '/webdavtests/copy3/', hdrs={'Destination': destination})
+
+        if res.status > 299:
+            raise Exception(f'COPY status code: {res.status}')
+
+        res = self.http.doRequest('GET', '/webdavtests/copy3_target/res0')
+        if res.status != 200:
+            raise Exception(f'GET failed: {res.status}')
+
+        if res.body != b'copy testcontent 1':
+            raise Exception('wrong content')
