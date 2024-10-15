b54964107169f25e2f1a802a525660a5c8799146
Olaf Wintermann <olaf.wintermann@gmail.com>
2024-10-15 20:33:42 +0200

    add move/overwrite test

--- davtest/tests/move.py
+++ davtest/tests/move.py
@@ -75,3 +75,33 @@ class TestMove(davtest.test.WebdavTest):
             numResponses=1)
         assertProperty(ms.get_first_response(), ns, 'myprop', content='testvalue1', status=200)
 
+    def test_move_overwrite(self):
+        self.create_testdata('move4', 1)
+        self.create_testdata('move4_target', 2)
+
+        res = self.http.doRequest('PUT', '/webdavtests/move4/res0', 'move4')
+        if res.status > 299:
+            raise Exception(f'cannot create test resource: {res.status}')
+
+        res = self.http.doRequest('PUT', '/webdavtests/move4_target/res0', 'move4target')
+        if res.status > 299:
+            raise Exception(f'cannot create test resource: {res.status}')
+
+        destination = self.http.get_uri('/webdavtests/move4_target/')
+        res = self.http.doRequest('MOVE', '/webdavtests/copy4/', hdrs={'Destination': destination, 'Overwrite': 'T'})
+
+        if res.status > 299:
+            raise Exception(f'MOVE status code: {res.status}')
+
+        exists = davtest.webdav.resource_exists(self.http, '/webdavtests/move4_target/res1')
+        if exists:
+            raise Exception('res1 should not exist')
+
+        res = self.http.doRequest('GET', '/webdavtests/move4/res0')
+        if res.status != 200:
+            raise Exception(f'GET failed: {res.status}')
+
+        if res.body != b'move4':
+            raise Exception('wrong content')
+
+
