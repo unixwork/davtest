d85e89ec285bfd0aa2301518b8f37c9b701398c6
Olaf Wintermann <olaf.wintermann@gmail.com>
2024-10-27 19:51:34 +0100

    add lock/put test

--- davtest/tests/lock.py
+++ davtest/tests/lock.py
@@ -26,9 +26,28 @@ class TestLock(davtest.test.WebdavTest):
 
         res = self.http.httpXmlRequest('UNLOCK', '/webdavtests/lock1/res0', lock_request1, hdrs={'Lock-Token': f'<{locktoken}>'})
 
-        if res.status > 299:
-            raise Exception(f'UNLCOK failed: {res.status}')
+        # usually UNLOCK should respond with 204, however the spec doesn't
+        # say that 200 (with response body) is forbidden
+        if res.status != 204 and res.status != 200:
+            raise Exception(f'UNLCOK: unexpected status code: {res.status}')
 
+    def test_lock_put(self):
+        self.create_testdata('lock2', 1)
+
+        res = self.http.httpXmlRequest('LOCK', '/webdavtests/lock2/res0', lock_request1, hdrs={'Timeout': 'Second-10'})
+
+        if res.status != 200:
+            raise Exception(f'LOCK failed: {res.status}')
+
+        lockdiscovery = davtest.webdav.LockDiscovery(res.body)
+        locktoken = lockdiscovery.locks[0].locktoken
+
+        res = self.http.doRequest('PUT', '/webdavtests/lock2/res0', 'new content', hdrs={'If': f'(<{locktoken}>)'})
+        if res.status > 299:
+            raise Exception(f'PUT failed: {res.status}')
+
+        if res.status >= 299:
+            raise Exception(f'UNLCOK failed: {res.status}')
 
 
 
